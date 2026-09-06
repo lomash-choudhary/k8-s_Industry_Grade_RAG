d0e73f10e40e703db1eabc9c11ffb1a45d0d6859
lomash-choudhary <lomashchoudhary9812@gmail.com>
2026-09-06 16:14:17 +0530

    fix: updated comments and made some fixes in the html parser

--- app/ingestion/loaders/html.py
+++ app/ingestion/loaders/html.py
@@ -16,18 +16,19 @@ def parse_html(file_path: str):
             with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
                 content = f.read()
 
+            # html.parser is the parser which we are using to convert the data
             soup = BeautifulSoup(content, "html.parser")
 
             #1. Remove Junk (Scripts, Styles, Meta Data)
 
-            for script in soup(["scripts", "style", "meta","noscript"]):
+            for script in soup(["script", "style", "meta","noscript"]):
                 script.decompose()
 
-            #2. Extract Text
+            # 2. Extract Text
+            # beautifulSoup will put a new line character when it moves from one tag to another tag
             text = soup.get_text(separator="\n")
 
             #3. Clean white spaces and collaps multilple newlines
-
             lines = (line.strip() for line in text.splitlines())
             chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
             text_clean = '\n'.join(chunk for chunk in chunks if chunk)
